3218538ee4f177f4b7b9529f018008501c7f436d
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2013-02-24 16:57:33 +0100

    Attributes: add attributes.

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+default['xhprof']['domain'] = 'xhprof'
+default['xhprof']['url'] = 'http://xhprof:4567'
+
+default['xhprof']['database']['host'] = 'localhost'
+default['xhprof']['database']['user'] = 'root'
+default['xhprof']['database']['pass'] = ''
+default['xhprof']['database']['name'] = 'xhprof'
